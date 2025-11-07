35aa6d1e2f76644a7c691521e9b22b9524688f8c
starryeye <see2818@naver.com>
2025-11-07 23:55:14 +0900

    [starryeye, 2025.11.07] - distributed transaction, monolithic
    내용
    - distributed transaction, monolithic

---
+++ monolithic/src/test/java/dev/starryeye/monolithic/MonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
