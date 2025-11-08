a595e576f36ac9a4f384b180a3fe10370ab636b8
starryeye <see2818@naver.com>
2025-11-08 17:51:07 +0900

    [starryeye, 2025.11.08] - distribute transaction, monolithic
    내용
    - point

---
+++ monolithic/src/main/java/dev/starryeye/monolithic/point/infrastructure/PointRepository.java
@@ -0,0 +1,11 @@
+package dev.starryeye.monolithic.point.infrastructure;
+
+import dev.starryeye.monolithic.point.domain.Point;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PointRepository extends JpaRepository<Point, Long> {
+
+    Optional<Point> findByUserId(Long userId);
+}
