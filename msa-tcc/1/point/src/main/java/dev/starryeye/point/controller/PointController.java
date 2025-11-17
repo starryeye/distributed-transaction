0c9295e610e1b4fa769d4c5a03b0acaad1d4486f
starryeye <see2818@naver.com>
2025-11-17 15:51:32 +0900

    [starryeye, 2025.11.17] - distribute transaction, msa-tcc
    내용
    - msa-tcc

--- msa-tcc/1/point/src/main/java/dev/starryeye/point/controller/PointController.java
+++ msa-tcc/1/point/src/main/java/dev/starryeye/point/controller/PointController.java
@@ -41,6 +41,23 @@ public class PointController {
 
     @PostMapping("/point/confirm")
     public void confirm(@RequestBody ReservedPointConfirmRequest request) {
-        pointService.confirmReserve(request.toCommand());
+
+        /**
+         * 모종의 이유로 동시에 동일한 reservationId 의 요청이 2회 이상 들어오면
+         * 막을 수 있도록 분산락을 적용하였다.
+         * 그리고, 예약 단계, 예약 취소 단계와 동시에 실행되는 것을 방지하고자 예약 단계에서 사용된 key 를 사용한다.
+         */
+
+        Boolean acquiredLock = lock.tryLock("point_reservation:", request.reservationId());
+
+        if (!acquiredLock) {
+            throw new RuntimeException("failed to acquire lock.. this is a duplicate request. another request is being processed.. reservationId = " + request.reservationId());
+        }
+
+        try {
+            pointService.confirmReserve(request.toCommand());
+        } finally {
+            lock.unlock("point_reservation:", request.reservationId());
+        }
     }
 }
