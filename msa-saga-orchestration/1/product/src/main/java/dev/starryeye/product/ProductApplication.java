dcefc4df8ecce2915054d1af4549ea32e97bff0b
starryeye <see2818@naver.com>
2025-11-21 23:31:16 +0900

    [starryeye, 2025.11.21] - distribute transaction, msa
    내용
    - msa

---
+++ msa-saga-orchestration/1/product/src/main/java/dev/starryeye/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
