2598126f5421a48549b298d1a47977245b6975c0
starryeye <see2818@naver.com>
2025-11-24 21:30:58 +0900

    [starryeye, 2025.11.24] - distribute transaction, msa-saga-orchestration
    내용
    - msa-saga-orchestration

--- msa-saga-orchestration/1/order/src/main/java/dev/starryeye/order/infrastructure/point/PointApiClient.java
+++ msa-saga-orchestration/1/order/src/main/java/dev/starryeye/order/infrastructure/point/PointApiClient.java
@@ -2,14 +2,30 @@ package dev.starryeye.order.infrastructure.point;
 
 import dev.starryeye.order.infrastructure.point.request.CancelUsedPointRequest;
 import dev.starryeye.order.infrastructure.point.request.UsePointRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.client.RestClient;
 
 @RequiredArgsConstructor
 public class PointApiClient {
 
+    /**
+     * retry
+     * 네트워크 문제나 일시적인 장애가 발생할 경우나 타임아웃이 발생했지만 실제로는 정상 처리가 되었을 경우에
+     * 곧바로 실패처리를 하기보다는 재시도를 통해 오류를 극복시키고 정상 처리로 유도하는 편이 바람직하여 설정함.
+     *
+     * circuitbreaker
+     * api 실패율이 높을때 외부 server 를 차단하기 위함
+     * fallback 은 딱히 필요 없이 예외가 발생하도록 둔다.
+     */
+    private static final String POINT_API_CB = "pointApiCb"; // circuitbreaker
+    private static final String POINT_API_RETRY = "pointApiRetry"; // retry
+
     private final RestClient restClient;
 
+    @Retry(name = POINT_API_RETRY)
+    @CircuitBreaker(name = POINT_API_CB)
     public void usePoint(UsePointRequest request) {
         restClient.post()
                 .uri("/point/use")
@@ -18,6 +34,8 @@ public class PointApiClient {
                 .toBodilessEntity();
     }
 
+    @Retry(name = POINT_API_RETRY)
+    @CircuitBreaker(name = POINT_API_CB)
     public void cancelUsedPoint(CancelUsedPointRequest request) {
         restClient.post()
                 .uri("/point/cancel")
--- msa-saga-orchestration/1/order/src/main/java/dev/starryeye/order/infrastructure/product/ProductApiClient.java
+++ msa-saga-orchestration/1/order/src/main/java/dev/starryeye/order/infrastructure/product/ProductApiClient.java
@@ -4,14 +4,21 @@ import dev.starryeye.order.infrastructure.product.request.BuyProductRequest;
 import dev.starryeye.order.infrastructure.product.request.CancelBoughtProductRequest;
 import dev.starryeye.order.infrastructure.product.response.BuyProductResponse;
 import dev.starryeye.order.infrastructure.product.response.CancelBoughtProductResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.client.RestClient;
 
 @RequiredArgsConstructor
 public class ProductApiClient {
 
+    private static final String PRODUCT_API_CB = "productApiCb"; // circuitbreaker
+    private static final String PRODUCT_API_RETRY = "productApiRetry"; // retry
+
     private final RestClient restClient;
 
+    @Retry(name = PRODUCT_API_RETRY)
+    @CircuitBreaker(name = PRODUCT_API_CB)
     public BuyProductResponse buyProduct(BuyProductRequest request) {
         return restClient.post()
                 .uri("/product/buy")
@@ -20,6 +27,8 @@ public class ProductApiClient {
                 .body(BuyProductResponse.class);
     }
 
+    @Retry(name = PRODUCT_API_RETRY)
+    @CircuitBreaker(name = PRODUCT_API_CB)
     public CancelBoughtProductResponse cancelBoughtProduct(CancelBoughtProductRequest request) {
         return restClient.post()
                 .uri("/product/buy/cancel")
